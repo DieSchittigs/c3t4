31ebc39e3281d4c8bae54bf337c3296a9f31cd65
Christopher Mühl <christopher@dieschittigs.de>
2018-03-20 10:59:18 +0100

    Copy fixtures and modules

--- fixtures/src/ContaoManager/Plugin.php
+++ fixtures/src/ContaoManager/Plugin.php
@@ -8,6 +8,9 @@ use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\CoreBundle\ContaoCoreBundle;
 
+/**
+ * @see https://github.com/contao/manager-plugin/blob/master/src/Bundle/BundlePluginInterface.php Code in GitHub
+ */
 class Plugin extends BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
